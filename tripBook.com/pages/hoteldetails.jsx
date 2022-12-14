b7fc18a286a15ca4c72a8a98d56464425068f688
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2022-12-14 21:55:21 +0530

    done

---
+++ tripBook.com/pages/hoteldetails.jsx
@@ -0,0 +1,30 @@
+import { Box,Text , Breadcrumb,BreadcrumbItem,BreadcrumbLink} from '@chakra-ui/react'
+import {ChevronRightIcon } from "@chakra-ui/icons"
+import React from 'react'
+
+const HotelDetails = () => {
+  return (
+   <Box  >
+    <Box bg='#fff0e0' border='none' color='black' p="1rem">
+    <Text fontSize='1xl' fontWeight="700" w="70%" m="auto" color="#262626"  >Coronavirus (COVID-19) Support</Text>
+    </Box>
+<Box w="80%" m="auto">
+  <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' />} >
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+  </BreadcrumbItem>
+
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>About</BreadcrumbLink >
+  </BreadcrumbItem >
+
+  <BreadcrumbItem isCurrentPage>
+    <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+  </BreadcrumbItem>
+</Breadcrumb>
+</Box>
+   </Box>
+  )
+}
+
+export default HotelDetails
--- tripBook.com/pages/index.js
+++ tripBook.com/pages/index.js
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../components/Footer'
 
 export default function Home() {
