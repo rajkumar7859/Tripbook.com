66a66709a50f8df8603e426571e5664628d61809
AdityaBarshilia <barshiliaaditya7@gmail.com>
2022-12-16 20:25:43 +0530

    crud/property

---
+++ tripBook.com/pages/api/property/[id].js
@@ -0,0 +1,57 @@
+import { connect } from "../../../db.connect";
+import { PropertyModel } from "../../../models/property.model";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  const { id } = req.query;
+
+  try {
+    await connect();
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (method === "GET") {
+    try {
+      const property = await PropertyModel.findOne({ _id: id });
+      res.status(200).send({ property });
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  }
+
+  if (method === "DELETE") {
+    try {
+      const property = await PropertyModel.findOneAndDelete({ _id: id });
+      res.status(200).send({ message: "Deleted", property });
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  }
+
+  if (method === "PUT") {
+    try {
+      const property = await PropertyModel.findOneAndReplace(
+        { _id: id },
+        req.body,
+        { new: true }
+      );
+      res.status(200).send({ message: "Replaced", property });
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  }
+
+  if (method === "PATCH") {
+    try {
+      const property = await PropertyModel.findOneAndUpdate(
+        { _id: id },
+        req.body,
+        { new: true }
+      );
+      res.status(200).send({ message: "Updated", property });
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  }
+}
--- tripBook.com/pages/api/property/index.js
+++ tripBook.com/pages/api/property/index.js
@@ -3,6 +3,7 @@ import { PropertyModel } from "../../../models/property.model";
 
 export default async function handler(req, res) {
   const { method } = req;
+  const { city, limit, page, sortBy, sortOrder } = req.query;
 
   try {
     await connect();
@@ -10,12 +11,27 @@ export default async function handler(req, res) {
     console.log(e);
   }
 
-  if(method === "POST"){
-    try{
-        const prop = await PropertyModel.create(req.body);
-        res.status(201).send({message: "property added successfully", prop});
-    }catch(e){
-        res.status(400).send(e.message);
+  if (method === "GET") {
+    try {
+      let properties = await PropertyModel.find();
+      if (city) {
+        properties = await PropertyModel.find({ city });
+      }
+
+      res.status(200).send({ properties });
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  }
+
+  if (method === "POST") {
+    try {
+      const properties = await PropertyModel.create(req.body);
+      res
+        .status(201)
+        .send({ message: "property added successfully", properties });
+    } catch (e) {
+      res.status(400).send(e.message);
     }
   }
 }
