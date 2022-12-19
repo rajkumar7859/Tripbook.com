c3ab5543a9430636ca57418f6f2479023822dfde
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2022-12-19 16:39:13 +0530

    nav

--- tripBook.com/pages/carrental.js
+++ tripBook.com/pages/carrental.js
@@ -1,4 +1,5 @@
 import { Slideshow } from "../components/explore/slideShow";
+import Navbar from "../components/navbarSection/navbar";
 import SearchBox from "../components/searchBox/searchBox";
 
 function carRental() {
@@ -32,6 +33,7 @@ function carRental() {
 
   return (
     <div>
+      <Navbar />
       <SearchBox />
       <Slideshow
         image={stay}
