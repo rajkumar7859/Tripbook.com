a4ac1aab8e731594a45dd74c09ecfc5bed09e5ab
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2022-12-19 20:24:58 +0530

    api

--- tripBook.com/pages/property/index.js
+++ tripBook.com/pages/property/index.js
@@ -131,7 +131,7 @@ const Property = ({ data }) => {
 export const getServerSideProps = async (context) => {
   const { city, sortBy, filterBy } = context.query;
   const res = await axios.get(
-    `/api/property?city=${city}&sortBy=${sortBy}&filterBy=${filterBy}`
+    `/property?city=${city}&sortBy=${sortBy}&filterBy=${filterBy}`
   );
 
   return {
